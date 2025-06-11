2ff6fd3f081a3369aa34d6dcb409debb1be9efcd
JefferPistala <jefferson.pistala@epn.edu.ec>
2025-06-11 17:04:25 -0500

    Eliminar import innecesario

--- src/main/java/madstodolist/service/UsuarioService.java
+++ src/main/java/madstodolist/service/UsuarioService.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
-import java.util.stream.Collectors;
+
 
 @Service
 public class UsuarioService {
